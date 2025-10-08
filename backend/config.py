0a4080c27193ea6c309386c6275d9ab81b9fc1c1
bastos9601 <bastoshugo01@gmail.com>
2025-10-08 02:08:49 -0500

    actualizando para que sea clonado

--- backend/config.py
+++ backend/config.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 # Configuración de la aplicación
 SECRET_KEY = os.getenv("SECRET_KEY", "tu-clave-secreta-muy-segura-aqui-cambiar-en-produccion")
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sistema_notas.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///../sistema_notas.db")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
